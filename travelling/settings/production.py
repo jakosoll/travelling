d4f621c293126c8ab54f28f1d6bbddcb3ab2c76d
Evgeiy Krivenko <jako.soll@gmail.com>
2020-10-06 15:50:51 +0300

    Update production.py

--- travelling/settings/production.py
+++ travelling/settings/production.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', 'tempsecret_adfjfqakdjgkldhglkakdsf')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
