9eb026bf9f3d50886bfb34edf7ac6d5b0df100d1
Evgeiy Krivenko <jako.soll@gmail.com>
2020-10-06 14:37:53 +0300

    Update settings

--- travelling/settings/production.py
+++ travelling/settings/production.py
@@ -25,13 +25,13 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'hadjfadkfaldgadkgadkgewrgsdgsf'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evgeniy-k-travelling.herokuapp.com']
 
 
 # Application definition
